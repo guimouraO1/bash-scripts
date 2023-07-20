c3ce88de4c498dc237a9a7bd99e5577a6cc633a8
Guilherme <101018681+guimouraO1@users.noreply.github.com>
2023-07-20 07:50:31 -0300

    Update aws_para_Numem.sh

--- Repor dias com problema/aws_para_Numem.sh
+++ Repor dias com problema/aws_para_Numem.sh
@@ -1,51 +1,36 @@
 #!/bin/bash
 
+
 ###########################################################################
-#        Script para copiar arquivos para uma pasta em nuvem GOES-16      #
+#                        Script para repor dias perdidos                  #
 ###########################################################################
 #  Metodo: Diario                                                         #
-#  Descricao: Script para copiar apenas o dia anterior para uma pasta em  #
-#  nuvem GOES-16                                                          #
+#  Descricao: Copia por meio de rclone da aws para a nuvem level2         #
 #  Autor: Guilherme de Moura Oliveira  <guimoura@unicamp.br>              #
-#  Data: 9/05/2023                                                        #
-#  Atualizacao: 18/05/2023                                                #
+#  Data: 09/07/2023                                                       #
+#  Atualizacao: 20/07/2023                                                #
 ###########################################################################
 
-ARQ_LOG="bkp_level2-nuvem`date +%Y-%m-%d`.log"
+# AWS -> Nuvem
 
-DIR_LOG=C:/Users/Gui/OneDrive/Documentos/copiar/logs/
+ARQ_LOG="copiando-AWS-Nvm`date +%Y-%m-%d`.log"
+DIR_LOG=/home/guimoura/logs/rclone/
 
-#Obtendo o ano e dia anterior em dia juliano
-ano=$(date +%Y -d "-1 day")
+# Obtendo o ano
+ano=2023
 
-dia=131
+# Loop para copiar os dias julianos de 001 a 005
+for ((dia=52; dia<=55; dia++))
+do
+    # Formata o número do dia juliano com três dígitos
+    dia_juliano=$(printf "%03d" $dia)
 
-# Diretório de origem
-origem="aws:/noaa-goes16/ABI-L1b-RadF/2023/$dia"
+    # Diretório de origem
+    origem="aws:/noaa-goes16/ABI-L1b-RadF/$ano/$dia_juliano"
 
-# Diretório de destino
-destino="level1b_2:/ano$ano/dia$dia/"
+    # Diretório de destino
+    destino="level1b_2:/ano$ano/dia$dia_juliano/"
 
-#Inicia o script
-echo "=========================================================================================================" &>> $DIR_LOG/$ARQ_LOG
-echo "=========================================================================================================" &>> $DIR_LOG/$ARQ_LOG
-echo "                            INICIANDO SCRIPT BACKUP PARA O DRIVE $(date '+%Y-%m-%d %H:%M:%S')            " &>> $DIR_LOG/$ARQ_LOG
-echo "=========================================================================================================" &>> $DIR_LOG/$ARQ_LOG
-echo "=========================================================================================================" &>> $DIR_LOG/$ARQ_LOG
-echo "" &>> $DIR_LOG/$ARQ_LOG
-echo "" &>> $DIR_LOG/$ARQ_LOG
-
-
-# Move o arquivo para o diretório de destino
-echo "Rclone size Ano: $ano Dia: $dia" >> $DIR_LOG$ARQ_LOG
-rclone size $origem --log-file=$DIR_LOG$ARQ_LOG -v >> $DIR_LOG$ARQ_LOG
-rclone copy $origem $destino --transfers=100 --log-level INFO --log-file=$DIR_LOG$ARQ_LOG
-# rclone copy $origem $destino --transfers=100 --dry-run --log-level INFO --log-file=$DIR_LOG$ARQ_LOG
-
-echo "=========================================================================================================" >> "$DIR_LOG/$ARQ_LOG"
-echo "=========================================================================================================" >> "$DIR_LOG/$ARQ_LOG"
-echo "=                                 FINALIZANDO O SCRIPT $(date '+%Y-%m-%d %H:%M:%S')                     =" >> "$DIR_LOG/$ARQ_LOG"
-echo "=========================================================================================================" >> "$DIR_LOG/$ARQ_LOG"
-echo "=========================================================================================================" >> "$DIR_LOG/$ARQ_LOG"
-echo "" &>> $DIR_LOG/$ARQ_LOG
-echo "" &>> $DIR_LOG/$ARQ_LOG
+    # Executa o comando rclone copy
+    rclone copy "$origem" "$destino" --transfers=100 --log-file="$DIR_LOG$ARQ_LOG" -v
+done
